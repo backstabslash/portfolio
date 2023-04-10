96c456b82ce167e6cf1897efa7ced2f91903dc8e
backstabslash <mr.toppanamor1@gmail.com>
2023-04-10 09:49:41 +0300

    mobilenav + themetoggle feelsgoodman

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import "../styles/modern-normalize.css";
 import "../styles/style.css";
+import "../styles/components/header.css";
+import "../styles/components/mobile-nav.css";
 import "../styles/components/hero.css";
 import "../styles/components/about.css";
 import "../styles/components/featured.css";
@@ -7,4 +9,9 @@ import "../styles/components/projects.css";
 import "../styles/components/contact.css";
 import "../styles/components/footer.css";
 import "../styles/utils.css";
-import "../styles/components/header.css";
+
+import mobileNav from "./utils/mobile-nav";
+import themeToggle from "./utils/themeToggle";
+
+mobileNav();
+themeToggle();
