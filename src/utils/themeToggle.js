28b90307f39eca794b9dfa598300e4df91f9e0de
backstabslash <mr.toppanamor1@gmail.com>
2023-04-20 20:13:33 +0300

    light main

--- src/utils/themeToggle.js
+++ src/utils/themeToggle.js
@@ -6,10 +6,10 @@ const themeToggle = () => {
   theme && document.body.classList.add(theme);
 
   const handleThemeToggle = () => {
-    document.body.classList.toggle("light-mode");
+    document.body.classList.toggle("dark-mode");
 
-    if (document.body.classList.contains("light-mode")) {
-      localStorage.setItem("theme", "light-mode");
+    if (document.body.classList.contains("dark-mode")) {
+      localStorage.setItem("theme", "dark-mode");
     } else {
       localStorage.removeItem("theme");
       document.body.removeAttribute("class");
